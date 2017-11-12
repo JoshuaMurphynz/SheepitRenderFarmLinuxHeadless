2c33bf343cd71a7f321ac2917b728b5360c9760d
joshua <joshuaglennmurphy@gmail.com>
2017-11-12 18:37:29 +1300

    Update Sheepit.sh
    Fixed formating

--- Sheepit.sh
+++ Sheepit.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 # Script to Download and run Sheepit render client
 # By Josh
-# Version .04 "Goners"
+# Version .05
 
-
-# todo: add colours
-	# echo -e "\033[31m Hello World"
-	# The [31m controls the text color: 30-37 sets foreground color, 40-47 sets background colour
-
-
+# Possible additions: Add Colours
 
 # Variables
-
 UsernameVariable=""
 PasswordVariable=""
 
-
 # Functions
-
 function waitFunction {
 sleep .9
 }
-
 
 # Actual code
 
@@ -29,45 +20,32 @@ printf "How many cores do you want to use, you have the option of using: "
 grep -c ^processor /proc/cpuinfo
 read -r NoC
 echo "Sweet, using $NoC cores"
-
-
 waitFunction
 echo "Alright, making a folder"
 cd /tmp || exit
 echo "Check if it exists first"
-
 if [ ! -d sheepitfolder ] ; then
 	mkdir sheepitfolder
-	echo "It doesn't lemme make it"
+	echo "It doesn't let me make it"
 fi
-
 waitFunction
 echo "Let's go into it"
 cd sheepitfolder || exit
 waitFunction
 echo "Let's make a temp folder for this terminal session only"
-
 TEMP=$(mktemp /tmp/sheepit_randomfolder.XXXXXXXX --directory)
 cd ${TEMP} || exit
-
 waitFunction
-
-
 echo "Done"
 waitFunction
-
 echo "Now to download the client"
 wget --quiet -O sheepit-clientTMPLATEST.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
 echo "Finished downloading"
-
 java -jar sheepit-clientTMPLATEST.jar --version
 waitFunction
-
 echo "Ready"
 waitFunction
-
 echo "Ctrl + C to stop"
-
 mkdir /tmp/sheepit-cache
 java -jar sheepit-clientTMPLATEST.jar -login "$UsernameVariable" -password "$PasswordVariable" -ui text -cores "$NoC" -cache-dir /tmp/sheepit-cache
 cd
